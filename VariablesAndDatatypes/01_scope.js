70600b5f1934e525cf40a1aacb2023eca69f83c8
Sehrish Noreen <sehrishnoreen31@gmail.com>
2025-05-03 08:38:13 +0500

    Variables Data Types

---
+++ VariablesAndDatatypes/01_scope.js
@@ -0,0 +1,29 @@
+// global scope
+var name = 'Sehrish';
+let degree = 'Software Engineering';
+const ID = 'GFHFJ45';
+
+
+
+// block scope
+{
+    var nameBlock = 'Sehrish Noreen';
+    let degreeBlock = 'BSEE';
+    const IDBlock = 'GFHFJ45';
+
+}
+
+// print global vars
+console.log(name);
+console.log(degree);
+console.log(ID);
+
+// print block vars
+console.log(nameBlock);
+console.log(IDBlock);
+console.log(degreeBlock);
+
+
+
+
+
